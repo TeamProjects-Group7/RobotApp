216bd9f2ff3e1bcd0a5894cb3bd098e440cb0331
thornywoods <brhdeal@gmail.com>
2022-01-30 23:16:32 -0500

    Added several screens and working buttons. Made UI slightly more appealing but still in Beta phase.

--- PythonApplication1/RobotApp.py
+++ PythonApplication1/RobotApp.py
@@ -1,9 +1,24 @@
 import kivy
 from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 
+
+class mainMenu(Screen):
+    pass
+
+class robotFiles(Screen):
+    pass
 
 class controlApp(App):
-    pass
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(mainMenu(name='menu'))
+        sm.add_widget(robotFiles(name='files'))
+
+        return sm
  
 kv = controlApp()
 kv.run()
--- README.md
+++
@@ -1,2 +0,0 @@
-# RobotApp
-Wright State University Team Projects 2022 robot control app
