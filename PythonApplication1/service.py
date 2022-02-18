4c52214a3d1c02c9cdaa2327b7e655dd83b54ee1
tw <brhdeal@gmail.com>
2022-02-18 16:17:44 -0500

    added new service functionality

--- PythonApplication1/service.py
+++ PythonApplication1/service.py
@@ -4,9 +4,9 @@ from plyer import notification
 
 
 PythonService = autoclass('org.kivy.android.PythonService')
-PythonService.mService.setAutoRestartService(True)
+#PythonService.mService.setAutoRestartService(True)
 print("service Started")
 
-while True:
-    notification.notify(title='service', message=str("Testing"))
+for x in range(6):
+    notification.notify(title='myService', message=str("Testing"))
     time.sleep(3)
